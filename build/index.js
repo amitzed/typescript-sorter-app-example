2480f8b22eedbf95765e6474321bbf2e5729b9ef
amitzed <amitzmn@gmail.com>
2020-12-28 15:38:04 -0500

    After importing Sorter into other class files and extending the other class files as child to Sorter - sorting still correctly works

--- build/index.js
+++ build/index.js
@@ -1,22 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
 var LinkedList_1 = require("./LinkedList");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-numbersCollection.sort();
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 // const charactersCollection = new CharactersCollection('zyxwvutsrqponmlkjihgfedcba');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data)
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-var sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-numbersCollection.sort();
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('zyxwvutsrqponmlkjihgfedcba');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data)
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -20,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
